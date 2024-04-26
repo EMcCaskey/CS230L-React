572974091ca2214d48acb3a99beeac7cd5c0b870
EMcCaskey <eam00027@mix.wvu.edu>
2024-04-26 12:24:06 -0400

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Navbar from './navbar.js'; 
+import Card from './cards.js';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1> <strong> CS 230L </strong> </h1>
           <h2> <strong> Section - 001</strong> </h2>
           <p> WVU ID 800382676 </p>
           <p> Hi I am Elizabeth McCaskey </p>
+          <Card/>
     </div>
   );
 }
